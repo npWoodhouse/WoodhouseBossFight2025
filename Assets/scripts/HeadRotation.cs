b6bdf85150ef7461479827184245aaf9bb13a146
npWoodhouse <167826411+npWoodhouse@users.noreply.github.com>
2025-10-30 14:42:24 -0600

    added dashing and jumping

--- Assets/scripts/HeadRotation.cs
+++ Assets/scripts/HeadRotation.cs
@@ -1,18 +1,27 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
 
 public class HeadRotation : MonoBehaviour
 {
     private PlayerInput playerInput;
+    public Rigidbody RB;
     private Vector2 lookVector;
     private Vector2 moveVector;
 
     public GameObject neck;
-    public float movespeed = 5.0f;
+    private float movespeed = 8.0f;
+    private float jumpforce = 400f;
+    private float dashdorce = 400f;
+
+    private bool canDash = true;
+    private bool canJump = true;
 
     void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
+        RB = GetComponent<Rigidbody>();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -28,7 +37,18 @@ public class HeadRotation : MonoBehaviour
         moveVector = playerInput.actions["Move"].ReadValue<Vector2>();
 
         neck.transform.rotation = Quaternion.Euler(0, euler_angle(-lookVector.x, lookVector.y), 0);
-        transform.position += new Vector3(moveVector.x*Time.deltaTime* movespeed, 0, moveVector.y*Time.deltaTime* movespeed);
+        transform.position += new Vector3(moveVector.x * Time.deltaTime * movespeed, 0, moveVector.y * Time.deltaTime * movespeed);
+
+
+        if (playerInput.actions["Dash"].triggered)
+        {
+            dash();
+        }
+
+        if (playerInput.actions["Jump"].triggered)
+        {
+            jump();
+        }
     }
 
     float euler_angle(float x, float y)
@@ -38,5 +58,39 @@ public class HeadRotation : MonoBehaviour
         if (x < 0) deg += 180;        // fixed mirrored angle of arctan
         float eul = (270 + deg) % 360;    // folded to [0,360) domain
         return eul;
+    }
+
+    void dash()
+    {
+        if (canDash)
+        {
+            Vector3 Impulse = new Vector3(moveVector.x, 0, moveVector.y);
+            RB.AddForce(Impulse * dashdorce, ForceMode.Impulse);
+            canDash = false;
+            StartCoroutine(DashCoolDown());
+        }
+    }
+
+    IEnumerator DashCoolDown()
+    {
+        yield return new WaitForSeconds(0.5f);
+        canDash = true;
+    }
+
+    void jump()
+    {
+        if (canJump)
+        {
+            Vector3 impulse = new Vector3(0, 1, 0);
+            RB.AddForce(impulse * jumpforce, ForceMode.Impulse);
+            canJump = false;
+            StartCoroutine(JumpCoolDown());
+        }
+    }
+
+    IEnumerator JumpCoolDown()
+    {
+        yield return new WaitForSeconds(0.5f);
+        canJump = true;
     }
 }
